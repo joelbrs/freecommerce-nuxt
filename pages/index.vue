2f0d0b1af09ce4dc53f2b0bf14f2ff31ba1a4704
Joel Filho <joelfilho02812@gmail.com>
2023-01-03 11:26:05 -0300

    projeto criado & add: navbar

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue';
+
+export default {
+  name: 'IndexPage',
+
+  components: { NavBar }
+}
+</script>
