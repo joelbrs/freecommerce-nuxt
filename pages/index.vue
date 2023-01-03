3182febe6b0024f78b578fab397f3f94b6dfacbd
Joel Filho <joelfilho02812@gmail.com>
2023-01-03 11:35:53 -0300

    add: footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <NavBar/>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
 import NavBar from '~/components/NavBar.vue';
+import TheFooter from '~/components/TheFooter.vue';
 
 export default {
   name: 'IndexPage',
 
-  components: { NavBar }
+  components: { NavBar, TheFooter }
 }
 </script>
